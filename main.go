b4dcc6a27f79cb5098e9f12b320fbc1c0a555e17
keithzano <keithzano@gmail.com>
2025-05-18 16:10:34 +0200

    Add 404 catch

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	fmt.Fprint(w, "<h1>This is the home page</h1>")
 }
 
