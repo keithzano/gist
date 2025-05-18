2e1e95a173ce94bd02c2cffd4489f785a1099df1
keithzano <keithzano@gmail.com>
2025-05-18 21:29:29 +0200

    Refactored the code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/view", view)
+	mux.HandleFunc("/create", create)
+
+	fmt.Println("Staring Server on port: 4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
