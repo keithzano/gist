1ec0b6c801c8438867bbcf0fe1dc8f74ca4e325e
keithzano <keithzano@gmail.com>
2025-05-18 15:38:27 +0200

    Basic http server to check if everyting is working

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>This is the home page</h1>")
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	fmt.Println("The server is starting on port :4000")
+	http.ListenAndServe(":4000", mux)
+
+}
