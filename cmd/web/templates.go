c108e5ae62781c4df433fbb601c42240ab1e1723
keithzano <keithzano@gmail.com>
2025-06-02 11:16:31 +0200

    Add Caching template

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,40 @@
 package main
 
-import "gist/internal/models"
+import (
+	"gist/internal/models"
+	"html/template"
+	"path/filepath"
+)
 
 type templateData struct {
 	Gist  *models.Gist
 	Gists []*models.Gist
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(".ui/html/pages/*html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/partials/nav.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
